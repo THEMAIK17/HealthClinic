7243c4a5c56776c2404451e239636fc62f597015
armando maikol duran salas <maikold774@gmail.com>
2025-10-06 15:58:30 -0500

    feat: the models for project was created

---
+++ Models/DocumenType.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealtClinic.Models;
+
+public class DocumenType
+{
+    public Guid Id { get; set; } = Guid.NewGuid(); // Unique ID for each document type
+    public string DocumentName { get; set; }             
+
+
+    public DocumenType(string documentName)
+    {
+        DocumentName = documentName;
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,57 @@
+using System.Reflection.Metadata;
+using HealtClinic.Models;
+using PetSystem.Models;
+namespace PatientSystem.Models;
+
+public abstract class Person:DocumenType
+{
+    private string address;
+    private string phone;
+
+    public Guid Id { get; set; }
+    public String Name { get; set; }
+
+    public String LastName { get; set; }
+    public String Document { get; set; }
+    public String Email { get; set; }
+    public Byte Age { get; set; }
+    public  String Address
+    {
+        get => address;
+        set => address = value;
+    }
+    public String Phone
+    {
+        get => phone;
+        set => phone = value;
+    }
+
+    public Person(  string name,
+                    string lastname,
+                    string documentName,
+                    string document,
+                    string email,
+                    byte age,
+                    string address,
+                    string phone): base(documentName)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        LastName = lastname;
+        DocumentName = documentName;
+        Document = document;
+        Email = email;
+        Age = age;
+        Address = address;
+        Phone = phone;
+    }
+
+
+    // Constructor
+
+    // Override ToString to display patient info
+    public virtual void ToString()
+    {
+        Console.WriteLine($"Id: {Id}, Name: {Name},Lastname: {LastName}, DocumentType: {DocumentName}, Document: {Document}, Email: {Email}, Age: {Age}, Address: {Address}, Phone: {Phone}");
+    }
+}
